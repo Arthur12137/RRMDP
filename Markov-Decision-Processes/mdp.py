5d2795e27d1336185ea28f85db7a66aee56201de
sunbolin <sunbolinmarvin@gmail.com>
2022-05-22 15:45:31 -0400

    ...:

--- Markov-Decision-Processes/mdp.py
+++ Markov-Decision-Processes/mdp.py
@@ -16,26 +16,26 @@ if len(sys.argv)>2:
 else:
     epsilon = 0.001
 
-def read_file():
-    #read transitions from file and store it to a variable
-    with open('transitions.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        for row in reader:
-            if row[0] in Transitions:
-                if row[1] in Transitions[row[0]]:
-                    Transitions[row[0]][row[1]].append((float(row[3]), row[2]))
-                else:
-                    Transitions[row[0]][row[1]] = [(float(row[3]), row[2])]
-            else:
-                Transitions[row[0]] = {row[1]:[(float(row[3]),row[2])]}
-
-    #read rewards file and save it to a variable
-    with open('rewards.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        for row in reader:
-            Reward[row[0]] = float(row[1]) if row[1] != 'None' else None
-
-read_file()
+# def read_file():
+#     #read transitions from file and store it to a variable
+#     with open('transitions.csv', 'r') as csvfile:
+#         reader = csv.reader(csvfile, delimiter=',')
+#         for row in reader:
+#             if row[0] in Transitions:
+#                 if row[1] in Transitions[row[0]]:
+#                     Transitions[row[0]][row[1]].append((float(row[3]), row[2]))
+#                 else:
+#                     Transitions[row[0]][row[1]] = [(float(row[3]), row[2])]
+#             else:
+#                 Transitions[row[0]] = {row[1]:[(float(row[3]),row[2])]}
+#
+#     #read rewards file and save it to a variable
+#     with open('rewards.csv', 'r') as csvfile:
+#         reader = csv.reader(csvfile, delimiter=',')
+#         for row in reader:
+#             Reward[row[0]] = float(row[1]) if row[1] != 'None' else None
+#
+# read_file()
 
 class MarkovDecisionProcess:
 
@@ -112,11 +112,11 @@ def expected_utility(a, s, V):
 
 
 #call value iteration
-V = value_iteration()
-print 'State - Value'
-for s in V:
-    print s, ' - ' , V[s]
-pi = best_policy(V)
-print '\nOptimal policy is \nState - Action'
-for s in pi:
-    print s, ' - ' , pi[s]
+# V = value_iteration()
+# print 'State - Value'
+# for s in V:
+#     print s, ' - ' , V[s]
+# pi = best_policy(V)
+# print '\nOptimal policy is \nState - Action'
+# for s in pi:
+#     print s, ' - ' , pi[s]
--- RRT-1/pRRT.py
+++ RRT-1/pRRT.py
@@ -22,9 +22,6 @@ class Nodes:
         self.parent_x = []
         self.parent_y = []
 
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
 
 # check collision
 def collision(x1,y1,x2,y2):
@@ -56,6 +53,9 @@ def collision(x1,y1,x2,y2):
         print("When it is thrown, the value of x1: ", x1)
         print("When it is thrown, the value of x2: ", x2)
         print("When it is thrown: the value of (x2-x1)/100: ", (x2-x1)/100)
+    except IndexError:
+        print("Index error occurred...")
+        print("int(y[i]), int(x[i]): ", int(y[i]), int(x[i]))
 
 # check the  collision with obstacle and trim
 # def check_collision(x1,y1,x2,y2):
@@ -84,7 +84,7 @@ def check_collision(tx, ty, nearest_x, nearest_y):
             # d, _ = dist_and_angle(x1, y1, x2, y2)
             d, _ = dist_and_angle(tx, ty, nearest_x, nearest_y)
             if d < 5:
-                directCon = True 
+                directCon = True
             else:
                 directCon = False
 
@@ -96,15 +96,13 @@ def check_collision(tx, ty, nearest_x, nearest_y):
             nodeCon = True
 
     # return(x,y,directCon,nodeCon)
-    return directCon, nodeCon
-
+    return (directCon, nodeCon)
 
 # return dist and angle b/w new point and nearest node
 def dist_and_angle(x1,y1,x2,y2):
     dist = math.sqrt( ((x1-x2)**2)+((y1-y2)**2) )
     angle = math.atan2(y2-y1, x2-x1)
-    return dist, angle
-
+    return(dist,angle)
 
 # return the neaerst node index
 def nearest_node(x,y):
@@ -131,6 +129,7 @@ def force_generation():
     direction_sampled = directions[direct_idx]
     return direction_sampled, magnitudes
 
+
 def steer_towards(nearest_x, nearest_y, nx, ny, step_size):
     dis, angle = dist_and_angle(nx, ny, nearest_x, nearest_y)
     direction_sampled, magnitudes = force_generation()
@@ -153,33 +152,16 @@ def steer_towards(nearest_x, nearest_y, nx, ny, step_size):
     return tx, ty
 
 
-def sample_neighbours_and_add_edge(x_new: Nodes, radius):
-    """
-    Samples all the neighbour nodes within a neighbourhood circle of radius 'radius'.
-    """
-    neighbours = []
-    for i in range(len(node_list)):
-        temp_node = node_list[i]
-        dist, _ = dist_and_angle(x_new.x, x_new.y, temp_node.x, temp_node.y)
-        if dist <= radius and temp_node != x_new:
-            if not collision(x_new.x, x_new.y, temp_node.x, temp_node.y):
-                x_new.parent_x.append(temp_node.x)
-                x_new.parent_y.append(temp_node.y)
-                temp_node.parent_x.append(x_new.x)
-                temp_node.parent_y.append(x_new.y)
-
-
-def RRG(img, img2, start, end, stepSize):
+def pRRT(img, img2, start, end, stepSize):
     h,l= img.shape # dim of the loaded image
     # print(img.shape) # (384, 683)
     # print(h,l)
+    max_len = 50
 
-    # neighbouring radius used in RRG algorithm
-    neighbour_radius = 65
-
+    particle_num = 10
 
     # insert the starting point in the node class
-    # node_list = [0] # list to store all the node points         
+    # node_list = [0] # list to store all the node points
     node_list[0] = Nodes(start[0],start[1])
     node_list[0].parent_x.append(start[0])
     node_list[0].parent_y.append(start[1])
@@ -191,32 +173,30 @@ def RRG(img, img2, start, end, stepSize):
     i=1
     pathFound = False
 
-    while not pathFound:
-        nx, ny = rnd_point(h, l)
-        print("Random points:", nx, ny)
+    # node_to_be_added = Nodes(tx, )
 
-        # Uncertainty here
-        # direction_sampled, magnitudes = force_generation()
-        # x_force, y_force = direction_sampled
-        # x_magnitude, y_magnitude = magnitudes
-        # x_diff, y_diff = x_force * x_magnitude, y_force * y_magnitude
-        # nx += x_force
-        # ny += y_force
+
+    while not pathFound:
+        nx,ny = rnd_point(h,l)
+        print("Random points:",nx,ny)
 
         nearest_ind = nearest_node(nx,ny)
-
+        # nearest_x = node_list[nearest_ind].x
+        # nearest_y = node_list[nearest_ind].y
+        # print("Nearest node coordinates:",nearest_x,nearest_y)
         nearest = node_list[nearest_ind]
 
         nearest_x = nearest.x
         nearest_y = nearest.y
-        print("Nearest node coordinates:",nearest_x,nearest_y)
 
         # Here we do the steering
         # tx, ty = steer_towards(nx, ny, nearest_x, nearest_y, stepSize)
         tx, ty = steer_towards(nearest_x, nearest_y, nx, ny, stepSize)
         print("type of tx: ", type(tx))
         print("type of ty: ", type(ty))
+
         node_to_be_added = Nodes(tx, ty)
+
         # check direct connection
         # tx,ty,directCon,nodeCon = check_collision(nx,ny,nearest_x,nearest_y)
         directCon, nodeCon = check_collision(np.float64(tx), np.float64(ty), nearest_x, nearest_y)
@@ -230,12 +210,8 @@ def RRG(img, img2, start, end, stepSize):
             node_to_be_added.parent_y = nearest.parent_y.copy()
             node_to_be_added.parent_x.append(nearest_x)
             node_to_be_added.parent_y.append(nearest_y)
-
-            # To make it into RRG: extension needs to add a couple of more edges
-            nearest.parent_x.append(tx)
-            nearest.parent_y.append(ty)
-
-            sample_neighbours_and_add_edge(node_to_be_added, neighbour_radius)
+            # node_list.append(i)
+            # node_list[i] = Nodes(tx,ty)
             # node_list[i].parent_x = node_list[nearest_ind].parent_x.copy()
             # node_list[i].parent_y = node_list[nearest_ind].parent_y.copy()
             # node_list[i].parent_x.append(tx)
@@ -246,7 +222,7 @@ def RRG(img, img2, start, end, stepSize):
             cv2.line(img2, (int(tx),int(ty)), (end[0],end[1]), (255,0,0), thickness=2, lineType=8)
 
             print("Path has been found")
-            #print("parent_x",node_list[i].parent_x)
+            # print("parent_x",node_list[i].parent_x)
             for j in range(len(node_list[i].parent_x)-1):
                 cv2.line(img2, (int(node_list[i].parent_x[j]),int(node_list[i].parent_y[j])), (int(node_list[i].parent_x[j+1]),int(node_list[i].parent_y[j+1])), (255,0,0), thickness=2, lineType=8)
             # cv2.waitKey(1)
@@ -255,26 +231,18 @@ def RRG(img, img2, start, end, stepSize):
             break
 
         elif nodeCon:
-
             node_list.append(node_to_be_added)
             node_to_be_added.parent_x = nearest.parent_x.copy()
             node_to_be_added.parent_y = nearest.parent_y.copy()
             node_to_be_added.parent_x.append(nearest_x)
             node_to_be_added.parent_y.append(nearest_y)
 
-            # Again: doing the RRG extension
-            # (The original code is really design in a weird way...)
-            nearest_node.parent_x.append(nearest_x)
-            nearest_node.parent_y.append(nearest_y)
-
-            sample_neighbours_and_add_edge(node_to_be_added, neighbour_radius)
-
             i=i+1
             # display
-            cv2.circle(img2, (int(tx), int(ty)), 2, (0, 0, 255), thickness=3, lineType=8)
-            cv2.line(img2, (int(tx), int(ty)), (int(node_list[nearest_ind].x), int(node_list[nearest_ind].y)), (0, 255, 0), thickness=1, lineType=8)
-            cv2.imwrite("media/"+str(i)+".jpg", img2)
-            cv2.imshow("sdc", img2)
+            cv2.circle(img2, (int(tx),int(ty)), 2,(0,0,255),thickness=3, lineType=8)
+            cv2.line(img2, (int(tx),int(ty)), (int(node_list[nearest_ind].x),int(node_list[nearest_ind].y)), (0,255,0), thickness=1, lineType=8)
+            cv2.imwrite("media/"+str(i)+".jpg",img2)
+            cv2.imshow("sdc",img2)
             cv2.waitKey(1)
             continue
 
@@ -289,6 +257,7 @@ def draw_circle(event,x,y,flags,param):
         cv2.circle(img2,(x,y),5,(255,0,0),-1)
         coordinates.append(x)
         coordinates.append(y)
+
 
 
 if __name__ == '__main__':
@@ -322,5 +291,5 @@ if __name__ == '__main__':
 
     coordinates=[]
 
-    # run the RRT algorithm 
-    RRG(img, img2, start, end, stepSize)
+    # run the RRT algorithm
+    RRT(img, img2, start, end, stepSize)
