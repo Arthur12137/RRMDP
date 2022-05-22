fad0c85a09fe9e8871ae1c6a5f86484831c2fd90
sunbolin <sunbolinmarvin@gmail.com>
2022-05-22 16:35:51 -0400

    ...

--- RRT-1/RRMDP.py
+++ RRT-1/RRMDP.py
@@ -5,7 +5,7 @@ class MarkovDecisionProcess:
         self.transition = {}
         self.cost = {}
 
-# States: a list of tuples
+# States: a list of tuples, (-1, -1) for dead-end state
 # Actions: a tuple of two tuples, denoting moving from one state to another
 # transition: a dictionary of...
 #
